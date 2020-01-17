1cb694cb954a94f27157d73fded2b96158fec5a5
ShanhinPavel <shanMail@tut.by>
2020-01-17 19:27:46 +0300

    feat: initialization of application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HeaderComponent } from "./components/common/header/header.component";
+import { FooterComponent } from "./components/common/footer/footer.component";
+import { HomeComponent } from "./components/home/home.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "about", component: FooterComponent },
+  { path: "**", component: HeaderComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FooterComponent } from "./components/common/footer/footer.component";
+import { HeaderComponent } from "./components/common/header/header.component";
+import { HomeComponent } from "./components/home/home.component";
+import { SearchSectionComponent } from "./components/home/home-content/search-section/search-section.component";
+import { HomeContentComponent } from "./components/home/home-content/home-content.component";
+import { TitleComponent } from "./components/common/title/title.component";
+import { MainSectionComponent } from "./components/home/home-content/main-section/main-section.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchSectionComponent,
+    HomeContentComponent,
+    TitleComponent,
+    MainSectionComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
