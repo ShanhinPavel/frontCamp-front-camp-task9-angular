089083909e0844d8e8beac5f422d9f0483a6e456
Pavel Shanhin <shanMail@tut.by>
2020-01-24 22:36:31 +0300

    feat: change components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FooterComponent } from './components/common/footer/footer.component';
-import { HeaderComponent } from './components/common/header/header.component';
-import { HomeComponent } from './components/home/home.component';
-import { SearchSectionComponent } from './components/home/search-section/search-section.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FooterComponent } from "./components/common/footer/footer.component";
+import { HeaderComponent } from "./components/common/header/header.component";
+import { HomeComponent } from "./components/home/home.component";
+import { SearchSectionComponent } from "./components/home/search-section/search-section.component";
 
-import { TitleComponent } from './components/common/title/title.component';
-import { MainSectionComponent } from './components/home/main-section/main-section.component';
-import { SelectComponent } from './components/common/select/select.component';
-import { InputComponent } from './components/common/input/input.component';
-import { CheckboxComponent } from './components/common/checkbox/checkbox.component';
-import { ButtonComponent } from './components/common/button/button.component';
-import { NewsLineComponent } from './components/home/main-section/news-line/news-line.component';
-import { PublishedDateComponent } from './components/common/published-date/published-date.component';
-import { ImageComponent } from './components/common/image/image.component';
-import { LoadMoreComponent } from './components/common/load-more/load-more.component';
-import { NewsCardComponent } from './components/news-details/news-card/news-card.component';
-import { NewsDetailsComponent } from './components/news-details/news-details.component';
-import { CreateEditArticleComponent } from './components/create-edit-article/create-edit-article.component';
-import { CreateEditArticleFormComponent } from './components/create-edit-article/create-edit-article-form/create-edit-article-form.component';
+import { TitleComponent } from "./components/common/title/title.component";
+import { MainSectionComponent } from "./components/home/main-section/main-section.component";
+import { SelectComponent } from "./components/common/select/select.component";
+import { InputComponent } from "./components/common/input/input.component";
+import { CheckboxComponent } from "./components/common/checkbox/checkbox.component";
+import { ButtonComponent } from "./components/common/button/button.component";
+import { NewsLineComponent } from "./components/home/main-section/news-line/news-line.component";
+import { PublishedDateComponent } from "./components/common/published-date/published-date.component";
+import { ImageComponent } from "./components/common/image/image.component";
+import { LoadMoreComponent } from "./components/home/main-section/load-more/load-more.component";
+import { NewsCardComponent } from "./components/news-details/news-card/news-card.component";
+import { NewsDetailsComponent } from "./components/news-details/news-details.component";
+import { CreateEditArticleComponent } from "./components/create-edit-article/create-edit-article.component";
+import { CreateEditArticleFormComponent } from "./components/create-edit-article/create-edit-article-form/create-edit-article-form.component";
 
 @NgModule({
   declarations: [
--- src/app/components/home/main-section/main-section.component.ts
+++ src/app/components/home/main-section/main-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 interface NewsLine {
   imageSrc: string;
@@ -7,29 +7,47 @@ interface NewsLine {
 }
 
 @Component({
-  selector: 'app-main-section',
-  templateUrl: './main-section.component.html',
-  styleUrls: ['./main-section.component.css']
+  selector: "app-main-section",
+  templateUrl: "./main-section.component.html",
+  styleUrls: ["./main-section.component.css"]
 })
 export class MainSectionComponent {
   newsLines: NewsLine[] = [
     {
       imageSrc:
-        'https://cnet2.cbsistatic.com/img/hEHxbI1Pphmvod56-B5ATDBmlwY=/0x0:2438x1440/980x551/2019/10/16/db85bfaa-b206-4e8e-a468-d878d80d46cc/newc-yongqing-bao-wildlife-photographer-of-the-year.jpg',
-      description: 'wild mouse runs from wolf',
-      publishedDate: '2020-01-18T19:21:09.322Z'
+        "https://cnet2.cbsistatic.com/img/hEHxbI1Pphmvod56-B5ATDBmlwY=/0x0:2438x1440/980x551/2019/10/16/db85bfaa-b206-4e8e-a468-d878d80d46cc/newc-yongqing-bao-wildlife-photographer-of-the-year.jpg",
+      description: "wild mouse runs from wolf",
+      publishedDate: "2020-01-18T19:21:09.322Z"
     },
     {
       imageSrc:
-        'https://cnet2.cbsistatic.com/img/hEHxbI1Pphmvod56-B5ATDBmlwY=/0x0:2438x1440/980x551/2019/10/16/db85bfaa-b206-4e8e-a468-d878d80d46cc/newc-yongqing-bao-wildlife-photographer-of-the-year.jpg',
-      description: 'wild mouse runs from wolf',
-      publishedDate: '2020-01-18T19:21:09.322Z'
+        "https://cnet2.cbsistatic.com/img/hEHxbI1Pphmvod56-B5ATDBmlwY=/0x0:2438x1440/980x551/2019/10/16/db85bfaa-b206-4e8e-a468-d878d80d46cc/newc-yongqing-bao-wildlife-photographer-of-the-year.jpg",
+      description: "wild mouse runs from wolf",
+      publishedDate: "2020-01-18T19:21:09.322Z"
     },
     {
       imageSrc:
-        'https://cnet2.cbsistatic.com/img/hEHxbI1Pphmvod56-B5ATDBmlwY=/0x0:2438x1440/980x551/2019/10/16/db85bfaa-b206-4e8e-a468-d878d80d46cc/newc-yongqing-bao-wildlife-photographer-of-the-year.jpg',
-      description: 'wild mouse runs from wolf',
-      publishedDate: '2020-01-18T19:21:09.322Z'
+        "https://cnet2.cbsistatic.com/img/hEHxbI1Pphmvod56-B5ATDBmlwY=/0x0:2438x1440/980x551/2019/10/16/db85bfaa-b206-4e8e-a468-d878d80d46cc/newc-yongqing-bao-wildlife-photographer-of-the-year.jpg",
+      description: "wild mouse runs from wolf",
+      publishedDate: "2020-01-18T19:21:09.322Z"
+    },
+    {
+      imageSrc:
+        "https://cnet2.cbsistatic.com/img/hEHxbI1Pphmvod56-B5ATDBmlwY=/0x0:2438x1440/980x551/2019/10/16/db85bfaa-b206-4e8e-a468-d878d80d46cc/newc-yongqing-bao-wildlife-photographer-of-the-year.jpg",
+      description: "wild mouse runs from wolf",
+      publishedDate: "2020-01-18T19:21:09.322Z"
+    },
+    {
+      imageSrc:
+        "https://cnet2.cbsistatic.com/img/hEHxbI1Pphmvod56-B5ATDBmlwY=/0x0:2438x1440/980x551/2019/10/16/db85bfaa-b206-4e8e-a468-d878d80d46cc/newc-yongqing-bao-wildlife-photographer-of-the-year.jpg",
+      description: "wild mouse runs from wolf",
+      publishedDate: "2020-01-18T19:21:09.322Z"
+    },
+    {
+      imageSrc:
+        "https://cnet2.cbsistatic.com/img/hEHxbI1Pphmvod56-B5ATDBmlwY=/0x0:2438x1440/980x551/2019/10/16/db85bfaa-b206-4e8e-a468-d878d80d46cc/newc-yongqing-bao-wildlife-photographer-of-the-year.jpg",
+      description: "wild mouse runs from wolf",
+      publishedDate: "2020-01-18T19:21:09.322Z"
     }
   ];
 }
