954ef9ee767c83dd1fed31fe71fe36eb0791e93c
Pavel Shanhin <shanMail@tut.by>
2020-01-26 01:47:20 +0300

    fix: correct path in router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { NewsDetailsComponent } from './components/news-details/news-details.component';
-import { CreateEditArticleComponent } from './components/create-edit-article/create-edit-article.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { NewsDetailsComponent } from "./components/news-details/news-details.component";
+import { CreateEditArticleComponent } from "./components/create-edit-article/create-edit-article.component";
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'article/:articleId', component: NewsDetailsComponent },
-  { path: 'create-edit', component: CreateEditArticleComponent }
+  { path: "", component: HomeComponent },
+  {
+    path: "article/create",
+    component: CreateEditArticleComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "article/:articleId",
+    component: NewsDetailsComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "article/:articleId/edit",
+    component: CreateEditArticleComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "**",
+    redirectTo: "",
+    pathMatch: "full"
+  }
 ];
 
 @NgModule({
