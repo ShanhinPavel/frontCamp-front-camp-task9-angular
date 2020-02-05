9f45b283576af8be01f7039951592789d7249462
Pavel Shanhin <shanMail@tut.by>
2020-02-06 01:01:54 +0300

    feat: add preview of picture when switch radio buttons

--- src/app/components/create-edit-article/create-edit-article-form/create-edit-article-form.component.ts
+++ src/app/components/create-edit-article/create-edit-article-form/create-edit-article-form.component.ts
@@ -1,32 +1,35 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { NewsArticle } from 'src/app/services/types';
-import { UpdateGroupRequestData } from '../../../services/types';
+import { Component, Input, OnInit } from "@angular/core";
+import { NewsArticle } from "src/app/services/types";
+import { UpdateGroupRequestData } from "../../../services/types";
 
 @Component({
-  selector: 'app-create-edit-article-form',
-  templateUrl: './create-edit-article-form.component.html',
-  styleUrls: ['./create-edit-article-form.component.css']
+  selector: "app-create-edit-article-form",
+  templateUrl: "./create-edit-article-form.component.html",
+  styleUrls: ["./create-edit-article-form.component.css"]
 })
 export class CreateEditArticleFormComponent {
   @Input() clickSave: (formFields: UpdateGroupRequestData) => void;
   @Input() clickCancel: () => void;
   @Input() articleDetails: NewsArticle;
+  private imgPreview: boolean;
 
   private formFields: UpdateGroupRequestData = {
-    title: '',
-    description: '',
-    publishedAt: '',
-    author: '',
-    urlToImage: '',
-    url: '',
-    content: ''
+    title: "",
+    description: "",
+    publishedAt: "",
+    author: "",
+    urlToImage: "",
+    url: "",
+    content: ""
   };
 
   ngOnInit() {
     this.formFields.title = this.articleDetails.title;
     this.formFields.description = this.articleDetails.description;
     this.formFields.content = this.articleDetails.content;
-    this.formFields.publishedAt = this.articleDetails.publishedAt;
+    this.formFields.publishedAt = new Date(
+      this.articleDetails.publishedAt
+    ).toLocaleDateString();
     this.formFields.urlToImage = this.articleDetails.urlToImage;
     this.formFields.author = this.articleDetails.author;
     this.formFields.url = this.articleDetails.url;
@@ -34,5 +37,5 @@ export class CreateEditArticleFormComponent {
 
   handleClickSave = () => {
     this.clickSave(this.formFields);
-  }
+  };
 }
